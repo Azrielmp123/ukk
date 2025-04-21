1cbfde7e4d33a51f51c0fb02e07f5f5796ee99e2
rasyaalam <rasyaalamsyah123@gmail.com>
2025-04-21 14:27:13 +0700

    firt

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\customers;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CustomerSeeder extends Seeder
+{
+    
+}
